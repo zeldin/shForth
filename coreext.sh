ce33877747a30628d9d30fcc099cf05a62e524b5
Fredrik Noring <noring@nocrew.org>
2017-06-16 22:50:00 +0200

    Add 0> core extension word

--- coreext.sh
+++ coreext.sh
@@ -62,3 +62,13 @@ zero_not_equals() { # 0<>
   fi
 }
 builtin '0<>' zero_not_equals
+
+zero_greater() { # 0>
+  pop n
+  if "$expr" 0 '<' "$n" >/dev/null; then
+    push -1
+  else
+    push 0
+  fi
+}
+builtin '0>' zero_greater
